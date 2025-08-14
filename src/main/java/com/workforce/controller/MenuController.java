41eeea3c574d7d13c614f43ab8b6d9438b6000e2
azhar <contact.ajharislam@gmail.com>
2025-08-14 18:05:33 +0600

    role menu permission

--- src/main/java/com/workforce/controller/MenuController.java
+++ src/main/java/com/workforce/controller/MenuController.java
@@ -11,10 +11,8 @@ import com.workforce.support.ApiResponseDto;
 import com.workforce.support.DeleteResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 
